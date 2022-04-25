161c97d2c61f9fe510393ebce509084a557aeda1
Ram <rmn24in@gmail.com>
2022-04-25 10:09:56 +0100

    config file updated

--- app/config.py
+++ app/config.py
@@ -11,7 +11,7 @@ TEMPLATES_FOLDER = "templates"
 FLASK_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 FLASK_ENV = os.environ.get("FLASK_ENV")
 
-API_KEY = os.environ.get("API_KEY") or os.environ.get("TEST_API_KEY")
-TEMPLATE_ID = os.environ.get("TEMPLATE_ID") or os.environ.get(
+API_KEY = os.environ.get("GOV_NOTIFY_API_KEY") or os.environ.get("TEST_API_KEY")
+TEMPLATE_ID = os.environ.get("GOV_NOTIFY_TEMPLATE_KEY") or os.environ.get(
     "TEST_TEMPLATE_ID"
 )
