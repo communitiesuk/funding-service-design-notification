11ed49b4a7048640e73e29b17e9b3ef9c7278cbd
Ram <rmn24in@gmail.com>
2022-04-26 08:58:51 +0100

    dummy email updated

--- app/notification/routes.py
+++ app/notification/routes.py
@@ -17,7 +17,7 @@ notifications_client = NotificationsAPIClient(API_KEY)
 
 @notification_bp.route("/", methods=["GET", "POST"])
 def send_notification():
-    email_address = os.environ.get("GOV_NOTIFY_DEFAULT_EMAIL_ADDRESS")
+    email_address = os.environ.get("test-funding-service-response@funding-service.com")
     template_id = TEMPLATE_ID
     response = notifications_client.send_email_notification(
         email_address=email_address,
