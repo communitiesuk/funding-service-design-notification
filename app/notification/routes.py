721490d1c2e6e3da811c6e95e8e2449bd0260d5b
Ram <rmn24in@gmail.com>
2022-04-26 11:15:36 +0100

    get post metthod removed

--- app/notification/routes.py
+++ app/notification/routes.py
@@ -1,6 +1,5 @@
-import os
-
 from app.config import API_KEY
+from app.config import EMAIL_ADDRESS
 from app.config import TEMPLATE_ID
 from flask import Blueprint
 from notifications_python_client.notifications import NotificationsAPIClient
@@ -15,9 +14,9 @@ notification_bp = Blueprint(
 notifications_client = NotificationsAPIClient(API_KEY)
 
 
-@notification_bp.route("/", methods=["GET", "POST"])
+@notification_bp.route("/")
 def send_notification():
-    email_address = os.environ.get("GOV_NOTIFY_DEFAULT_EMAIL_ADDRESS")
+    email_address = EMAIL_ADDRESS
     template_id = TEMPLATE_ID
     response = notifications_client.send_email_notification(
         email_address=email_address,
