1f505f5e1d449ca4589f4ad34206562635c3af77
Ram <rmn24in@gmail.com>
2022-05-04 21:29:45 +0100

    get_data func removed, get_example_data added

--- app/notification/models/data.py
+++ app/notification/models/data.py
@@ -4,18 +4,9 @@ import os
 from app.config import FLASK_ROOT
 
 
-def get_data(data) -> dict:
-    if data:
-        return data
-    else:
-        data = get_local_data()
-
-    return data
-
-
-def get_local_data() -> dict:
+def get_example_data() -> dict:
     local_data = os.path.join(
-        FLASK_ROOT, "tests", "local_data", "local_data.json"
+        FLASK_ROOT, "app", "notification", "models", "example_data.json"
     )
     with open(local_data) as notification_data:
         data = json.load(notification_data)
