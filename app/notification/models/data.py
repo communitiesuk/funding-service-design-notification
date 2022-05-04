4e02671f5a1144bd88b9f22a76cdd197c6540dc0
Ram <rmn24in@gmail.com>
2022-05-04 22:27:51 +0100

    local_data var name changed to example_data to align with naming convention

--- app/notification/models/data.py
+++ app/notification/models/data.py
@@ -5,9 +5,9 @@ from app.config import FLASK_ROOT
 
 
 def get_example_data() -> dict:
-    local_data = os.path.join(
+    example_data = os.path.join(
         FLASK_ROOT, "app", "notification", "models", "example_data.json"
     )
-    with open(local_data) as notification_data:
+    with open(example_data) as notification_data:
         data = json.load(notification_data)
         return data
