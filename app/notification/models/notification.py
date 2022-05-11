b878bc751b21aec65a7782006a8effa353134bfe
Ram <rmn24in@gmail.com>
2022-05-11 16:24:25 +0100

    Validation bug fixed,  updated with relevant error message if incorrect keys, values posted.

--- app/notification/models/notification.py
+++ app/notification/models/notification.py
@@ -1,8 +1,11 @@
+import json
 from dataclasses import dataclass
 
 from app.config import API_KEY
 from app.config import MAGIC_LINK_TEMPLATE_ID
+from app.notification.models.data import get_example_data
 from notifications_python_client.notifications import NotificationsAPIClient
+
 
 notifications_client = NotificationsAPIClient(API_KEY)
 
@@ -21,36 +24,36 @@ class Notification:
 
     @staticmethod
     def process_notification_data(data):
+        if "" not in data.values() and None not in data.values():
 
-        if (
-            (
-                "type" in data.keys()
-                and "to" in data.keys()
-                and "content" in data.keys()
-            )
-            and "" not in data.values()
-            and None not in data.values()
-        ):
             notification_data = Notification(
-                template_type=data.get("type"),
-                contact_info=data.get("to"),
-                content=data.get("content"),
+                template_type=data["type"],
+                contact_info=data["to"],
+                content=data["content"],
             )
 
             return notification_data
 
     @staticmethod
     def send_magic_link(json_data):
-        data = Notification.process_notification_data(json_data)
-        response = notifications_client.send_email_notification(
-            email_address=data.contact_info,
-            template_id=MAGIC_LINK_TEMPLATE_ID,
-            personalisation={
-                "SUBJECT": "Funding service access link",
-                "MAGIC_LINK": data.content,
-            },
-        )
-        return response
+        try:
+            data = Notification.process_notification_data(json_data)
+            response = notifications_client.send_email_notification(
+                email_address=data.contact_info,
+                template_id=MAGIC_LINK_TEMPLATE_ID,
+                personalisation={
+                    "SUBJECT": "Funding service access link",
+                    "MAGIC_LINK": data.content,
+                },
+            )
+            return response
+
+        except (TypeError, KeyError, AttributeError):
+            example_data = json.dumps(get_example_data(), indent=2)
+            return (
+                "Bad request, please check the contents of the notification"
+                f" data.\n\n Example data: {example_data}"
+            )
 
     @staticmethod
     def send_notification(contact_info, content):
