53d07dd68b38c6bdf5a69235474757c238aa376d
Ram <rmn24in@gmail.com>
2022-05-18 22:46:02 +0100

    Validation for types moved to new file

--- app/notification/models/notification.py
+++ app/notification/models/notification.py
@@ -4,8 +4,8 @@ from dataclasses import dataclass
 from app.config import API_KEY
 from app.config import MAGIC_LINK_TEMPLATE_ID
 from app.notification.models.custom_exceptions import NotificationError
-from app.notification.models.data import fund_name
 from app.notification.models.data import get_example_data
+from app.notification.models.process_contents import ProcessNotificationData
 from notifications_python_client.notifications import NotificationsAPIClient
 
 
@@ -25,9 +25,8 @@ class Notification:
     content: str
 
     @staticmethod
-    def process_notification_data(data):
+    def notification_data(data):
         if "" not in data.values() and None not in data.values():
-
             notification_data = Notification(
                 template_type=data["type"],
                 contact_info=data["to"],
@@ -38,14 +37,14 @@ class Notification:
 
     @staticmethod
     def send_magic_link(json_data):
-
+        process_json_data = ProcessNotificationData.fund_name(json_data)
         try:
-            data = Notification.process_notification_data(json_data)
+            data = Notification.notification_data(process_json_data)
             response = notifications_client.send_email_notification(
                 email_address=data.contact_info,
                 template_id=MAGIC_LINK_TEMPLATE_ID,
                 personalisation={
-                    "name of fund": fund_name(data.content["fund_name"]),
+                    "name of fund": data.content["fund_name"],
                     "link to application": data.content["magic_link_url"],
                     "contact details": (
                         "dummy_contact_info@funding-service-help.com"
