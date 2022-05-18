299a02dbbb8bcf50e9c6c4ec0808c847c0686dee
Ram <rmn24in@gmail.com>
2022-05-18 22:44:45 +0100

    Function moved to new files: - process_template_types - process_contents

--- app/notification/models/data.py
+++ app/notification/models/data.py
@@ -2,7 +2,6 @@ import json
 import os
 
 from app.config import FLASK_ROOT
-from app.notification.models.custom_exceptions import NotificationError
 
 
 def get_example_data() -> dict:
@@ -12,31 +11,3 @@ def get_example_data() -> dict:
     with open(example_data) as notification_data:
         data = json.load(notification_data)
         return data
-
-
-def fund_name(data):
-    if data is None or data == "":
-        return "Funds"
-    else:
-        return data
-
-
-def email_recipient(data: dict, example_data: dict, notification_class):
-    if data.get("type") == "MAGIC_LINK":
-        magic_link = notification_class.send_magic_link(data)
-        return magic_link
-    elif data.get("type") == "NOTIFICATION":
-        return f"Currently {data.get('type')} service is not available."
-    elif data.get("type") == "REMINDER":
-        return f"Currently {data.get('type')} service is not available."
-    elif data.get("type") == "AWARD":
-        return f"Currently {data.get('type')} service is not available."
-    else:
-        raise NotificationError(
-            message=(
-                "Incorrect type, please check the key 'type' & other keys,"
-                f" values from notification data: {data}.\n\nExpected"
-                " type:('MAGIC_LINK' or 'NOTIFICATION' or 'REMINDER' or"
-                f" 'AWARD' )\n\nExample data: {example_data}"
-            )
-        )
