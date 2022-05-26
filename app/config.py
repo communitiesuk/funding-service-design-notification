28941d4be497a04793bc494825910fd9848081b5
Ram <rmn24in@gmail.com>
2022-05-26 12:44:17 +0100

    Frontend components removed: config, manifest.yml & main.yml updated

--- app/config.py
+++ app/config.py
@@ -1,14 +1,10 @@
 """Flask configuration."""
 import os
 
-SECRET_KEY = os.environ.get("SECRET_KEY") or "dev"
-SESSION_COOKIE_NAME = os.environ.get("SESSION_COOKIE_NAME") or "session_cookie"
-STATIC_FOLDER = "static"
-TEMPLATES_FOLDER = "templates"
 FLASK_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 FLASK_ENV = os.environ.get("FLASK_ENV")
 
-# CURRENTLY TEST KEY IS BEING USED LOCALLY & ON CF.
+# CURRENTLY TEST API KEY IS BEING USED LOCALLY & ON CF.
 API_KEY = os.environ.get("GOV_NOTIFY_API_KEY") or os.environ.get(
     "TEST_API_KEY"
 )
