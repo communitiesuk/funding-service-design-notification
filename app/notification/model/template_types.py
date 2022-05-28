4a95e2e821072d42564b67ed514940d78e7eb019
Ram <rmn24in@gmail.com>
2022-05-28 18:58:01 +0100

    Notifier class is called inside template_types instead of routes.py

--- app/notification/model/template_types.py
+++ app/notification/model/template_types.py
@@ -1,26 +1,27 @@
 from app.notification.model.exceptions import NotificationError
+from app.notification.model.notifier import Notifier
 
 
-def email_recipient(data: dict, notification_class):
+def email_recipient(json_data: dict):
     """
     Function matches with the correct template type &
     calls the relevant function from process_data.py.
     """
-    match data.get("type"):
+    match json_data.get("type"):
         case "MAGIC_LINK":
-            return notification_class.send_magic_link(data)
+            return Notifier.send_magic_link(json_data)
 
         case "APPLICATION_RECORD_OF_SUBMISSION":
-            return notification_class.send_application(data)
+            return Notifier.send_application(json_data)
 
         case "NOTIFICATION" | "REMINDER" | "AWARD":
-            return f"Currently {data.get('type')} service is not available."
+            return f"Currently {json_data.get('type')} service is not available." # noqa
 
         case _:
             raise NotificationError(
                 message=(
                     "Incorrect type, please check the key 'type' & other keys,"
-                    f" values from notification data: {data}.\n\nExpected"
+                    f" values from notification data: {json_data}.\n\nExpected"
                     " type:('MAGIC_LINK' or 'NOTIFICATION' or 'REMINDER' or"
                     f" 'AWARD' or 'APPLICATION_RECORD_OF_SUBMISSION')." # noqa
                 )
