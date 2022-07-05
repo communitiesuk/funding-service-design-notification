dce14477d06f7726514cab3443f4c68df49daf83
Ram <rmn24in@gmail.com>
2022-07-05 17:01:58 +0100

    🔧  pattern match case updated

--- config/__init__.py
+++ config/__init__.py
@@ -11,8 +11,15 @@ match FLASK_ENV:
         from config.envs.development import DevelopmentConfig as Config
     case "unit_test":
         from config.envs.unit_test import UnitTestConfig as Config
+    case "dev":
+        from config.envs.dev import DevConfig as Config    
+    case "test":
+        from config.envs.test import TestConfig as Config   
+    case "production":
+        from config.envs.production import ProductionConfig as Config        
     case _:
         from config.envs.default import DefaultConfig as Config
+     
 
 try:
     Config.pretty_print()
