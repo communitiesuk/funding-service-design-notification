a0cc47f2cbd59bf000b49db287ae958bf7dee647
Ram <rmn24in@gmail.com>
2022-07-13 10:53:43 +0100

    ♻️  validaion removed as the json being validated by connexion

--- app/notification/model/notification.py
+++ app/notification/model/notification.py
@@ -4,29 +4,25 @@ from dataclasses import dataclass
 @dataclass
 class Notification:
     """
-    Class processes notification data.
+    Class processes notification json contents.
     Returns:
         notification class object.
     """
 
     template_type: str
     contact_info: str
-    content: str
+    content: dict
 
     @staticmethod
     def from_json(data):
         """
-        Function initialise the data by checking the required format
-        of the data which must contain keys("type", "content", "to")
-        & required values then
         Function will be  called in relevant services to map
-        the incoming data from external services otherwise return the
-        error by relevant service based on given "template_type".
+        the json contents
         """
-        if "" not in data.values() and None not in data.values():
-            notification_data = Notification(
-                template_type=data["type"],
-                contact_info=data["to"],
-                content=data["content"],
-            )
-            return notification_data
+
+        notification_data = Notification(
+            template_type=data.get("type"),
+            contact_info=data.get("to"),
+            content=data.get("content"),
+        )
+        return notification_data
