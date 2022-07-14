05909f9bb589a9739dedf55b55a802fb09d7e82c
Ram <rmn24in@gmail.com>
2022-07-14 16:00:23 +0100

    🥅  error response returns jsonify contents

--- app/notification/model/response.py
+++ app/notification/model/response.py
@@ -1,24 +1,43 @@
-from flask import Response
-    
+from flask import jsonify
+
+
 def magic_link_error(message):
-    return Response (
-        "Incorrect MAGIC LINK data, please check the contents of"
-        " the MAGIC LINK data. Example data:"
-        f" {message}"
-    ), 400
-            
+    return (
+        jsonify(
+            detail="Incorrect MAGIC LINK data",
+            status=400,
+            expected_data=f"{message}",
+        ),
+        400,
+    )
+
+
 def application_submission_error(message):
-    return Response (
-        "Incorrect APPLICATION data, please check the contents of"
-        " the APPLICATION data. \nExample data:"
-        f" {message}"
-    ), 400
+    return (
+        jsonify(
+            detail="Incorrect APPLICATION data",
+            status=400,
+            expected_data=f"{message}",
+        ),
+        400,
+    )
 
 
 def template_type_error(message):
-    return Response (
-        f"Incorrect type, please check the value of key 'type': {message.get('type')}. Expected"
-        " type:('MAGIC_LINK' or 'NOTIFICATION' or 'REMINDER' or"
-        f" 'AWARD' or 'APPLICATION_RECORD_OF_SUBMISSION')." 
-    ), 400
-         
+    return (
+        jsonify(
+            detail=(
+                "Incorrect type, please check the value of key 'type':"
+                f" {message.get('type')}"
+            ),
+            status=400,
+            expected_type=(
+                "MAGIC_LINK",
+                "NOTIFICATION",
+                "REMINDER",
+                "AWARD",
+                "APPLICATION_RECORD_OF_SUBMISSION",
+            ),
+        ),
+        400,
+    )
