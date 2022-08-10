f5bc04d764d34ea1ef99835f663a969bc8b5f17f
Ram <rmn24in@gmail.com>
2022-08-10 09:51:43 +0100

    🧱  MagicLink class created to simplify the structure

--- app/notification/magic_link/map_contents.py
+++ app/notification/magic_link/map_contents.py
@@ -1,20 +1,29 @@
+from dataclasses import dataclass
+
 from app.notification.model.exceptions import NotificationError
+from app.notification.model.notification import Notification
 from app.notification.model.response import magic_link_key_error
 from tests.test_magic_link.magic_link_data import (
     expected_magic_link_data,
 )
 
 
-class ProcessMagicLinkData:
-    """
-    Class is set up to map data
-    from notification data contents.
+@dataclass
+class MagicLink:
+    contact_info: str
+    fund_name: str
+    magic_link: str
+    contact_details: str = "dummy_contact_info@funding-service-help.com"
 
-    Returns: notification data or updated notification data
-    to map out with expected contents. If any of the key such as
-    "fund_name" is missing from expected contents, it adds the the
-    key eg "fund_name" and assign value to default value.
-    """
+    @staticmethod
+    def from_json(json_data: dict):
+        data = Notification.from_json(json_data)
+        fund = MagicLink.process_data(json_data)
+        return MagicLink(
+            contact_info=data.contact_info,
+            fund_name=fund["content"]["fund_name"],
+            magic_link=data.content["magic_link_url"],
+        )
 
     @staticmethod
     def process_data(data: dict):
