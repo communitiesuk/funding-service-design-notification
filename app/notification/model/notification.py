446141ccd8622f64372d366c0a7b5f63a7622ea3
Ram <rmn24in@gmail.com>
2022-09-08 20:59:26 +0100

    Simplified data process for each service & removes duplication of incoming data to avoid any errors

--- app/notification/model/notification.py
+++ app/notification/model/notification.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass
 
+from app.notification.application.map_contents import Application
+from app.notification.magic_link.map_contents import MagicLink
+from flask import current_app
+from fsd_utils.config.notify_constants import NotifyConstants
+
 
 @dataclass
 class Notification:
@@ -19,3 +24,28 @@ class Notification:
             content=data.get("content"),
         )
         return notification_data
+
+    @staticmethod
+    def process_data(json_data):
+        data = Notification.from_json(json_data)
+        match data.template_type:
+
+            case NotifyConstants.TEMPLATE_TYPE_MAGIC_LINK:
+                current_app.logger.debug(
+                    "Second step - connect data with MAGIC_LINK service class"
+                    " to  process contents"
+                )
+                return MagicLink.from_json(data)
+
+            case "APPLICATION_RECORD_OF_SUBMISSION":
+                current_app.logger.info(
+                    "Second step - connect data with APPLICATION service"
+                    " class to  process_data"
+                )
+                return Application.from_json(data)
+
+            case "NOTIFICATION" | "REMINDER" | "AWARD":
+                return f"Currently {data.get('type')} service is not available."  # noqa
+
+            case _:
+                return "Incorrect template type - testing"
