c8ca5ed85dfb456885ae1258e22e721d23d963de
Ram <rmn24in@gmail.com>
2022-09-08 20:52:20 +0100

    template type update using utils common_config constant

--- app/notification/model/template_types.py
+++ app/notification/model/template_types.py
@@ -1,5 +1,7 @@
 from app.notification.model.notifier import Notifier
 from app.notification.model.response import template_type_error
+from flask import current_app
+from fsd_utils.config.notify_constants import NotifyConstants
 
 
 def email_recipient(json_data: dict):
@@ -8,10 +10,14 @@ def email_recipient(json_data: dict):
     calls the relevant function from process_data.py.
     """
     match json_data.get("type"):
-        case "MAGIC_LINK":
+        case NotifyConstants.TEMPLATE_TYPE_MAGIC_LINK:
+            current_app.logger.info(
+                "First step - connect with MAGIC_LINK to send an email")
             return Notifier.send_magic_link(json_data)
 
         case "APPLICATION_RECORD_OF_SUBMISSION":
+            current_app.logger.info(
+                "First step - connect with APPLICATION to send an email")
             return Notifier.send_application(json_data)
 
         case "NOTIFICATION" | "REMINDER" | "AWARD":
