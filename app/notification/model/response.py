9cbc1d74b705d3860c085d6f0a73b30a2f6505e0
Ram <80714392+ramsharma-prog@users.noreply.github.com>
2022-09-12 21:49:20 +0100

    Validation refactoring: unused response messages removed

--- app/notification/model/response.py
+++ app/notification/model/response.py
@@ -1,45 +1,6 @@
 from flask import jsonify
 
 
-def magic_link_key_error(message: str, code: int = 400):
-    return (
-        jsonify(
-            detail="Incorrect MAGIC LINK data",
-            status=code,
-            error=f"{message}",
-        ),
-        code,
-    )
-
-
-def application_key_error(message: str, code: int = 400):
-    return (
-        jsonify(
-            detail="Incorrect or missing APPLICATION data",
-            status=code,
-            error=f"{message}",
-        ),
-        code,
-    )
-
-
-def invalid_data_error(
-    message: dict,
-    code: int = 400,
-):
-    return (
-        jsonify(
-            detail=(
-                "Incorrect data, check following contents that may contain"
-                " invalid email and incorrect or null values"
-            ),
-            status=code,
-            error=message,
-        ),
-        code,
-    )
-
-
 def template_type_error(message: str, code: int = 400):
     return (
         jsonify(
