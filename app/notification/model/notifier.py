bc95e69fc8d63b2ad64d6fbdf95ef1cdecd6ac85
Ram <80714392+ramsharma-prog@users.noreply.github.com>
2022-10-14 22:25:03 +0100

    Added logic to make a call to govuk-notify-service: with application reminder contents

--- app/notification/model/notifier.py
+++ app/notification/model/notifier.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from app.notification.application.map_contents import Application
+from app.notification.application_reminder.map_contents import (
+    ApplicationReminder,
+)
 from app.notification.magic_link.map_contents import MagicLink
 from app.notification.model.response import invalid_data_error
 from config import Config
@@ -125,12 +128,40 @@ class Notifier:
             )
 
     @staticmethod
-    def send_notification(json_data):
-        pass
+    def send_application_reminder(
+        notification: Notification, code: int = 200
+    ) -> tuple:
+        """Function makes a call to govuk-notify-service with mapped contents
+        that are expected by the govuk-notify-service template.
 
-    @staticmethod
-    def send_reminder(json_data):
-        pass
+        Args: Takes an instance of Notification class.
+
+        Raises HTTPError if any of the required contents are incorrect
+        or missing.
+        """
+        try:
+            contents = ApplicationReminder.from_notification(notification)
+
+            response = notifications_client.send_email_notification(
+                email_address=contents.contact_info,
+                template_id=Config.APPLICATION_DEADLINE_REMINDER_TEMPLATE_ID,
+                personalisation={
+                    "name of fund": contents.fund_name,
+                    "application reference": contents.reference,
+                    "round name": contents.round_name,
+                    "application deadline": contents.deadline_date,
+                },
+            )
+            current_app.logger.info("Call made to govuk Notify API")
+            return response, code
+
+        except errors.HTTPError:
+            current_app.logger.exception(
+                "HTTPError while sending notification"
+            )
+            return invalid_data_error(
+                Application.from_notification(notification)
+            )
 
     @staticmethod
     def send_award(json_data):
