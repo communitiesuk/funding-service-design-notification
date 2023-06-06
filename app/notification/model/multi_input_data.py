8646523d8c2377d28cfc1e41b79228b04f910719
Ram <80714392+ramsharma-prog@users.noreply.github.com>
2023-06-06 18:09:48 +0100

    FS-2969-submit-application-returns-internal-error (#87)
    * added logs to catch the error and:
    simplified some files for debugging
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- app/notification/model/multi_input_data.py
+++ app/notification/model/multi_input_data.py
@@ -1,6 +1,7 @@
 import uuid
 
 from app.notification.model.notification_utils import convert_bool_value
+from flask import current_app
 
 
 class MultiInput:
@@ -98,7 +99,7 @@ class MultiInput:
                     output.append(cls.format_values(value, index))
 
             # handles multiple nested values containing year, month formatting and others
-            except:  # noqa
+            except ValueError:  # noqa
                 if (
                     isinstance(value, list)
                     and len(value) > 0
@@ -115,5 +116,11 @@ class MultiInput:
                     output.append(
                         cls.format_keys_and_values(key, value, index)
                     )
+            except Exception as e:
+                current_app.logger.error(
+                    "Could not map the multi input data for the following"
+                    f" section: {data}",
+                    e,
+                )
 
         return output
