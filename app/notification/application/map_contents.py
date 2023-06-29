eefd6538acda2af66c1b493cfa9258ccf259f37b
Ram <80714392+ramsharma-prog@users.noreply.github.com>
2023-06-29 11:06:03 +0100

    FS-3036-map-application-q-a-for-text-file (#92)
    * file deleted -> functions moved to utils
    
    * importing functions from utils for application Q and A mapping & formatting
    
    * moved tests to utils that are related to application mapping and formatting functionalities
    
    * moved test data to util that is related to applications mapping and formatting functionalities
    
    * utils version bumped

--- app/notification/application/map_contents.py
+++ app/notification/application/map_contents.py
@@ -1,24 +1,20 @@
 from __future__ import annotations
 
-import collections
 from dataclasses import dataclass
 from datetime import datetime
 from io import BytesIO
-from io import StringIO
 from typing import TYPE_CHECKING
 
 import pytz
-from app.notification.application.application_utils import (
-    sort_questions_and_answers,
-)
-from app.notification.model.notification_utils import format_answer
-from app.notification.model.notification_utils import simplify_title
 from app.notification.notification_contents_base_class import (
     _NotificationContents,
 )
 from config import Config
 from flask import current_app
+from fsd_utils import extract_questions_and_answers
+from fsd_utils import generate_text_of_application
 from fsd_utils.config.notify_constants import NotifyConstants
+
 
 if TYPE_CHECKING:
     from app.notification.model.notification import Notification
@@ -88,14 +84,6 @@ class Application(_NotificationContents):
         return forms
 
     @classmethod
-    def get_form_names(cls, notification: Notification) -> list:
-        form_names = []
-        forms = cls.get_forms(notification)
-        for form in forms:
-            form_names.append(form.get(NotifyConstants.APPLICATION_NAME_FIELD))
-        return list(dict.fromkeys(form_names))
-
-    @classmethod
     def get_questions_and_answers(cls, notification: Notification) -> dict:
         """
         Extracts questions and answers from form data.
@@ -106,42 +94,15 @@ class Application(_NotificationContents):
         Returns:
             dict: A dictionary mapping form names to their respective questions and answers.
         """
-        questions_answers = collections.defaultdict(dict)
         forms = cls.get_forms(notification)
-        form_names = cls.get_form_names(notification)
-        questions_answers = sort_questions_and_answers(
-            forms, form_names, questions_answers
-        )
+
+        questions_answers = extract_questions_and_answers(forms)
         return questions_answers
 
     @classmethod
     def get_fund_name(cls, notification):
         metadata = notification.content[NotifyConstants.APPLICATION_FIELD]
         return metadata.get("fund_name")
-
-    @classmethod
-    def format_questions_answers_with_stringIO(
-        cls,
-        notification: Notification,
-    ) -> str:
-        """Function formats dict of questions/answers
-        for readability with StringIO."""
-        json_file = cls.get_questions_and_answers(notification)
-        output = StringIO()
-
-        output.write(
-            f"********* {cls.get_fund_name(notification)} **********\n"
-        )  # noqa
-
-        for section_name, values in json_file.items():
-
-            title = simplify_title(section_name, remove_text=["cof", "ns"])
-
-            output.write(f"\n* {' '.join(title).capitalize()}\n\n")
-            for questions, answers in values.items():
-                output.write(f"  Q) {questions}\n")
-                output.write(f"  A) {format_answer(answers)}\n\n")
-        return output.getvalue()
 
     @classmethod
     def bytes_object_for_questions_answers(
@@ -151,9 +112,9 @@ class Application(_NotificationContents):
         """Function creates a memory object for question & answers
         with ByteIO from StringIO.
         """
-        stringIO_data = cls.format_questions_answers_with_stringIO(
-            notification
-        )
+        q_and_a = cls.get_questions_and_answers(notification)
+        fund_name = cls.get_fund_name(notification)
+        stringIO_data = generate_text_of_application(q_and_a, fund_name)
         convert_to_bytes = bytes(stringIO_data, "utf-8")
         bytes_object = BytesIO(convert_to_bytes)
         return bytes_object
