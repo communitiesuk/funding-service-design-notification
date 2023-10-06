43ab95f2b7e329f96816b358b5611691349afc72
Ram <80714392+ramsharma-prog@users.noreply.github.com>
2023-10-06 09:21:23 +0100

    Added fund DPI template configuration and: (#126)
    - updated configuration for template INCOMPLETE_APPLICATION_TEMPLATE_ID  for each fund as currently template for fund COF is being used for all the funds.

--- config/envs/default.py
+++ config/envs/default.py
@@ -36,12 +36,31 @@ class DefaultConfig:
             "fund_name": "CYP",
             "template_id": "1c69f104-edfa-49d7-9bab-cbbd30c323f3",
         },
+        "f493d512-5eb4-11ee-8c99-0242ac120002": {
+            "fund_name": "DPI",
+            "template_id": "6e1d80ac-c843-4b47-9946-ecad542dd5de",
+        },
     }
 
-    INCOMPLETE_APPLICATION_TEMPLATE_ID = os.environ.get(
-        "INCOMPLETE_APPLICATION_TEMPLATE_ID",
-        "fc8cff7c-a595-4590-a1a4-eccda48d8604",
-    )
+    INCOMPLETE_APPLICATION_TEMPLATE_ID = {
+        "47aef2f5-3fcb-4d45-acb5-f0152b5f03c4": {
+            "fund_name": "COF",
+            "template_id": "fc8cff7c-a595-4590-a1a4-eccda48d8604",
+        },
+        "13b95669-ed98-4840-8652-d6b7a19964db": {
+            "fund_name": "NSTF",
+            "template_id": "d4c9cb6f-c36d-4157-a5e4-0b7bc323e332",
+        },
+        "1baa0f68-4e0a-4b02-9dfe-b5646f089e65": {
+            "fund_name": "CYP",
+            "template_id": "5e308d1e-4d1d-4572-9c99-9a8396224aed",
+        },
+        "f493d512-5eb4-11ee-8c99-0242ac120002": {
+            "fund_name": "DPI",
+            "template_id": "7980eee3-5a40-42b8-98dc-ba3b11ea4e65",
+        },
+    }
+
     APPLICATION_DEADLINE_REMINDER_TEMPLATE_ID = os.environ.get(
         "APPLICATION_DEADLINE_REMINDER_TEMPLATE_ID",
         "e41cc73d-6947-4cbb-aedd-4ab2f470a2d2",
