06812ce17ecd7bac80fda0e5b238131ae08f27ab
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2024-09-23 14:39:23 +0100

    fs-4647 update manifest, bump utils

--- config/envs/dev.py
+++ config/envs/dev.py
@@ -9,7 +9,6 @@ from config.envs.default import DefaultConfig as Config
 class DevConfig(Config):
     """Flask Dev Pipeline Environment Configuration."""
 
-    SECRET_KEY = "dev"  # pragma: allowlist secret
     SESSION_COOKIE_NAME = "session_cookie"
 
     # Logging
